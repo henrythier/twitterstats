dd01f90669571970989ff8d6e4aa3a80714c8c15
pippowie <phil.pp@me.com>
2021-12-29 23:14:54 +0100

    refactored to use api v2

--- app.py
+++ app.py
@@ -1,138 +1,135 @@
+import json
 import os
+import re
+from typing import ParamSpec
 from flask import Flask, render_template, redirect, url_for
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import requests
 from dateutil import parser
-import os
+
 
 app = Flask(__name__, static_url_path='',
             static_folder='static',
             template_folder='template')
 api = Api(app)
 
-# To set your enviornment variables in your terminal run the following line:
-bearer_token = os.getenv("TWITTER_BEARER")
 
-'''
-Start: Twitter functions
-'''
+BEARER = os.getenv("TWITTER_BEARER")
+BEARER = "AAAAAAAAAAAAAAAAAAAAAMgXXgEAAAAAsPs%2BsYjYHga2srwskxUNrVB8SWc%3DdR9s4KlBNHacRi1CmVti99KSvWw9cXok4g0KzTu1AeLBFF7uDf"
 
 
-# year
-relevant_year = 2021
+def bearer_oauth(r):
+    r.headers["Authorization"] = f"Bearer {BEARER}"
+    return r
 
 
 def get_user(username):
-    user_fields = "user.fields=profile_image_url"
-    headers = {"Authorization": f"Bearer {bearer_token}"}
-    url = f'https://api.twitter.com/2/users/by/?usernames={username}&{user_fields}'
-    r = requests.get(url, headers=headers)
+    params = {'user.fields': 'profile_image_url'}
+    url = f'https://api.twitter.com/2/users/by/username/{username}?'
+    r = requests.get(url, auth=bearer_oauth, params=params)
     if r.status_code != 200:
         raise Exception(f'Request returned an error: {r.status_code} {r.text}')
-    return r.json()
+    return r.json()['data']
 
 
-def get_tweets(params):
-    headers = {"Authorization": f"Bearer {bearer_token}"}
-    url = 'https://api.twitter.com/1.1/favorites/list.json?'
-    r = requests.get(url, headers=headers, params=params)
-    if r.status_code != 200:
-        raise Exception(f'Request returned an error: {r.status_code} {r.text}')
-    return r.json()
+def get_tweets(userid, params):
+    params['expansions'] = 'author_id'
+    params['user.fields'] = 'username'
+    params['tweet.fields'] = 'created_at,in_reply_to_user_id'
+    url = f'https://api.twitter.com/2/users/{userid}/liked_tweets'
+    # url = 'https://api.twitter.com/1.1/favorites/list.json?'
+    r = requests.get(url, auth=bearer_oauth, params=params)
+    return r.json(), r.status_code
 
 
-def get_relevant_info(response_dict):
-    ''' function to reduce twitters original response to relevant data '''
-    relevant_data_keys = ['created_at', 'id', 'text', 'in_reply_to_screen_name']
-    reduced_dict = {rel_key: response_dict[rel_key] for rel_key in relevant_data_keys}
-    reduced_dict['created_at'] = parser.parse(reduced_dict['created_at'])
-    reduced_dict['user'] = response_dict['user']['screen_name']
-    reduced_dict['tweet_url'] = f"twitter.com/{reduced_dict['user']}/status/{reduced_dict['id']}"
-    return reduced_dict
+def process_tweet(tweet):
+    ''' function to reduce tweets to relevant data '''
+    tweet = {
+        'created_at': parser.parse(tweet['created_at']),
+        'id': tweet['id'],
+        'reply_to': tweet.get('in_reply_to_user_id'),
+        'author_id': tweet['author_id']
+    }
+    return tweet
 
 
-def tweets_to_df(liked_tweets):
-    ''' function to turn reduced dict into dataframe and remove older likes '''
-    # move dict into dataframe
-    df = pd.DataFrame.from_dict(liked_tweets)
+def calc_stats(user, tweets, users):
+    num_likes = len(tweets)
+    num_accounts = len(tweets['author_id'].unique())
+    num_liked_replies = len(tweets[tweets['reply_to'] == user['id']])
+    share_liked_replies = num_liked_replies / num_likes * 100
+    first_like = tweets['created_at'].iloc[-1]
+    last_like = tweets['created_at'].iloc[0]
 
-    # only keep tweets from year in question
-    df_years = df['created_at'].apply(lambda x: x.year)
-    df = df[df_years == relevant_year]
-    return df
+    top_five = {users[k]: v for k, v in dict(tweets['author_id'].value_counts().head(5)).items()}
 
-
-def calc_and_print_stats(df, screen_name):
-    ''' function to calculate stats '''
-    # number of likes
-    num_of_tweets = len(df)
-
-    # number of liked accounts
-    num_of_different_accounts = len(df['user'].unique())
-
-    # number of liked replys
-    num_of_liked_replys = len(df.loc[df['in_reply_to_screen_name'] == screen_name])
-    per_liked_replys = num_of_liked_replys / num_of_tweets * 100
-
-    # top 10 accounts
-    top_ten = df['user'].value_counts().head(10)
-    stats = {'user': screen_name,
-             'num_of_likes': num_of_tweets,
-             'num_of_different_accounts': num_of_different_accounts,
-             'num_of_liked_replys': num_of_liked_replys,
-             'per_liked_replys': per_liked_replys,
-             'top_ten': top_ten.to_dict(),
-             'first_like': df['created_at'].iloc[-1],
-             'last_like': df['created_at'].iloc[0]}
+    stats = {
+        'user': user['username'],
+        'num_likes': num_likes,
+        'num_accounts': num_accounts,
+        'num_liked_replies': num_liked_replies,
+        'share_liked_replies': f"{share_liked_replies:.2f}%",
+        'top_five': top_five,
+        'first_like': f"{first_like:%Y-%m-%d}",
+        'last_like': f"{last_like:%Y-%m-%d}"
+    }
     return stats
 
 
-def get_like_stats(screen_name):
-    user = get_user(screen_name)
+def get_like_stats(username):
+    try:
+        user = get_user(username)
+    except Exception as e:
+        return None, 403
 
     # setup
-    parameters = {'count': 200}
-    parameters.pop("max_id", None)
-    parameters['screen_name'] = screen_name
-    liked_tweets = list()
-    iteration_count = 0
+    parameters = {}
+    likes = []
+    users = {}
+    i = 0
+    status = 200
 
-    # loop while still in relevant year
     while True:
+        raw_data, status_code = get_tweets(user['id'], parameters)
 
-        # get tweets
-        tweet_dict_raw = get_tweets(parameters)
+        if i > 0 and status_code == 429:
+            status = status_code
+            break
 
         # check for errors
-        if 'errors' in tweet_dict_raw:
+        if 'errors' in raw_data:
             return None, 403
 
         # check if account has likes
-        if iteration_count == 0 and len(tweet_dict_raw) == 0:
+        if i == 0 and len(raw_data) == 0:
             return None, 404
 
-        batch_of_liked_tweets = [get_relevant_info(tweet) for tweet in tweet_dict_raw]
-
-        # maximum number of likes retrieved for user
-        if len(batch_of_liked_tweets) <= 1 and iteration_count > 0:
-            print(f'End of likes after {iteration_count} requests')
+        likes_batch = raw_data.get('data')
+        if not likes_batch:
+            break
+        likes_batch = [process_tweet(tweet) for tweet in likes_batch if tweet['created_at'][:4] == '2021']
+        if len(likes_batch) == 0:
             break
 
-        # append to list of liked tweets
-        liked_tweets.extend(batch_of_liked_tweets)
+        likes.extend(likes_batch)
 
-        # update parameters
-        parameters['max_id'] = batch_of_liked_tweets[-1]['id'] - 1
-        iteration_count += 1
+        user_batch = {user['id']: user['username'] for user in raw_data['includes']['users']}
+        users.update(user_batch)
 
-        # check if still in relevant year
-        if relevant_year > liked_tweets[-1]['created_at'].year:
+        next_token = raw_data.get('meta', {}).get('next_token')
+        if next_token:
+            parameters['pagination_token'] = next_token
+        else:
             break
 
-    df = tweets_to_df(liked_tweets)
-    response = calc_and_print_stats(df, screen_name)
-    return response, 200
+        i += 1
+        #print(f'{i = }, {len(likes) = }')
+    
+    likes = pd.DataFrame.from_dict(likes)
+    stats = calc_stats(user, likes, users)
+
+    return stats, status
 
 
 @app.route("/")
@@ -142,13 +139,14 @@ def home():
 
 @app.route("/<name>")
 def user(name):
-    data, status = get_like_stats(name)
-    if status == 200:
-        return render_template('user.html', name=name, data=data)
-    elif status == 403:
-        return render_template('404.html', name=name)
-    else:
-        return render_template('error.html', name=name)
+    regexp = re.compile(r'^\w{1,15}$')
+    if regexp.search(name):
+        data, status = get_like_stats(name)
+        if status in (200, 429):
+            return render_template('user.html', name=name, data=data)
+        elif status == 403:
+            return render_template('404.html', name=name)
+    return render_template('error.html', name=name)
 
 
 if __name__ == '__main__':
